cb06ffae79deae87c7f8de5e5b65a470d480f4cf
SMarchenko <sam.marchenko@gmail.com>
2022-07-23 20:27:36 +0300

    Added finding start/finish checker's move positions

--- DZ_CheckersProject/Assets/Scripts/ClickController.cs
+++ DZ_CheckersProject/Assets/Scripts/ClickController.cs
@@ -12,12 +12,17 @@ namespace DefaultNamespace
         [SerializeField] private CheckerView[] _checkerViews;
         [SerializeField] private List<CellView> _cellNeighbors;
 
+        [SerializeField] private Transform startMovePosition;
+        [SerializeField] private Transform endMovePosition;
+        [SerializeField] private bool wantMove;
+
 
         private void Start()
         {
             foreach (var cell in _cellViews)
             {
-                cell.OnCellClick += GetNeighbors;
+                //cell.OnCellClick += GetNeighbors;
+                cell.OnCellClick += CanMove;
             }
 
             foreach (var checker in _checkerViews)
@@ -25,6 +30,7 @@ namespace DefaultNamespace
                 checker.OnCheckerClick += OnChecker;
             }
         }
+
         private void GetNeighbors(CellView cellView)
         {
             _cellNeighbors.Clear();
@@ -70,21 +76,44 @@ namespace DefaultNamespace
             }
         }
 
+        private void CanMove(CellView cellView)
+        {
+            
+            if (!wantMove)
+            {
+                startMovePosition = null;
+                endMovePosition = null;
+                return;
+            }
+            wantMove = false;
+            foreach (var cell in _cellNeighbors)
+            {
+                if (cellView == cell)
+                {
+                    endMovePosition = cell.transform;
+                    return;
+                }
+            }
+
+            startMovePosition = null;
+        }
+
         private void OnChecker(CheckerView checkerView)
         {
+            wantMove = true;
             Debug.Log("Test");
-            var xChecker = (int)checkerView.transform.position.x;
-            var zChecker = (int)checkerView.transform.position.z;
-            //checkerView.transform.position = new Vector3(xChecker, 0, zChecker);
-            
+            var xChecker = (int) checkerView.transform.position.x;
+            var zChecker = (int) checkerView.transform.position.z;
+
             foreach (var cell in _cellViews)
             {
-                var xCell = (int)cell.transform.position.x;
-                var zCell= (int)cell.transform.position.z;
-                //cell.transform.position = new Vector3(xCell, 0, zCell);
-                
+                var xCell = (int) cell.transform.position.x;
+                var zCell = (int) cell.transform.position.z;
+
                 if (xChecker == xCell && zChecker == zCell)
                 {
+                    startMovePosition = cell.transform;
+                    endMovePosition = null;
                     Debug.Log("In if");
                     GetNeighbors(cell);
                 }
